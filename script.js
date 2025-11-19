857146d8144813238f3d9e0ae42ff853a69fa7b1
reedmartin <reedmartin10@gmail.com>
2025-11-19 11:08:57 -0800

    logic for computer choice complete

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 // Start //
 
+/// function that randomly returns "rock", "paper", or scissors"
+// function, random number between 0 and 3 and rounds it
+function getComputerChoice() {
+    let randomNum = Math.random() * 3;
+    randomNum = Math.floor(randomNum);
+    
+    // evaluates what number was returned and assigns to either "Rock", "Paper", or "Scissors"
+    if (randomNum == 0) {
+        let computerChoice = "Rock"
+        return computerChoice;
+    }
+    else if (randomNum == 1) {
+        let computerChoice = "Paper"
+        return computerChoice;
+    }
+
+    else {
+        let computerChoice = "Scissors"
+        return computerChoice;
+    }
+}
+// tests to see if the computer choice is being generated and turned into an output we want //
+console.log(getComputerChoice())
+// human choice //
